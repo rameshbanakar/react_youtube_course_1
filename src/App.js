2df286a017b509c01d9aa106e3a6caf96ce34d9e
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-20 18:32:13 +0530

    functional componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./App.css"
+import FuncComp from './Components/FuncComp'
 const App = () => {
   return (
     <div>
-      <h1 className='App'>Hello world</h1>
+     <FuncComp/>
     </div>
   )
 }
---
+++ src/Components/FuncComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FuncComp() {
+  return (
+    <div>
+        <h3>This is Recat functional component</h3>
+    </div>
+  )
+}
+
+export default FuncComp
