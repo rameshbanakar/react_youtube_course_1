6e46e73fb39c9e3c89184442ec0ce60f7903297d
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-20 19:03:42 +0530

    Arrow comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./App.css"
 import FuncComp from './Components/FuncComp'
+import {ArrowComp} from "./Components/ArrowComp"
 const App = () => {
   return (
     <div>
-     <FuncComp/>
+     {/* <FuncComp/> */}
+     <ArrowComp/>
     </div>
   )
 }
---
+++ src/Components/ArrowComp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ArrowComp = () => {
+  return (
+    <div>
+       <h3>This is react arrow functional component</h3>
+    </div>
+  )
+}
+
