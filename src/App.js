30b505b6e925b409d70942cc1926c54937a21314
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-20 19:05:18 +0530

    class comp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import "./App.css"
 import FuncComp from './Components/FuncComp'
 import {ArrowComp} from "./Components/ArrowComp"
+import ClassComp from './Components/ClassComp'
 const App = () => {
   return (
     <div>
      {/* <FuncComp/> */}
      <ArrowComp/>
+     <ClassComp/>
     </div>
   )
 }
---
+++ src/Components/ClassComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClassComp extends Component {
+  render() {
+    return (
+      <div> 
+      <h3>This is class component</h3>
+      </div>
+    )
+  }
+}
