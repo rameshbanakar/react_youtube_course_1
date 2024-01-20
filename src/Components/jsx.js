f65ebfed6f52616e27251915309ae0a09d858fe0
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-20 20:15:12 +0530

    jsx using

--- src/Components/jsx.js
+++ src/Components/jsx.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function jsx() {
   return (
-   React.createElement("div",null,React.createElement('h1',null,"hello world"))
+   React.createElement("div",null,React.createElement('h1',{id:"hello",className:"dummyClass"},"hello world"))
   )
 }
 
 // jsx stands for javascript xml which help us to write the HTML code in the react application
+
+// jsx diff compare to html 
+
+// 1.class-->className
+// 2.for-->htmlFor 
+// 3.camleCase proprty naming convetion
+//    onclick-->onClick
