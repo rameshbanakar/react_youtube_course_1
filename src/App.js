f2782e65d1ba161f2d99d60e47f42ffccf218c6e
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-20 20:04:55 +0530

    jsx code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css"
 import FuncComp from './Components/FuncComp'
 import {ArrowComp} from "./Components/ArrowComp"
 import ClassComp from './Components/ClassComp'
+import Jsx from "./Components/jsx"
 const App = () => {
   return (
     <div>
-     {/* <FuncComp/> */}
-     <ArrowComp/>
-     <ClassComp/>
+      <Jsx />
+      {/* <FuncComp/> */}
+      {/* <ArrowComp/>
+     <ClassComp/> */}
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/Components/jsx.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function jsx() {
+  return (
+   React.createElement("div",null,React.createElement('h1',null,"hello world"))
+  )
+}
+
+// jsx stands for javascript xml which help us to write the HTML code in the react application
